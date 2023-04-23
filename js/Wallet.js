5b547e220464d0aaf5851c73c11fe162b20722f3
K4-R4 <mabdman512@gmail.com>
2023-04-23 21:25:08 +0900

    feat/wallet-class: added wallet class

---
+++ js/Wallet.js
@@ -0,0 +1,22 @@
+class Wallet{
+    constructor(money){
+        this.#money = money;
+    }
+    getMoney(){
+        return this.#money;
+    }
+    setMoney(money){
+        this.#money = money;
+    }
+    deposit(amount){
+        const amountToDeposit = Math.max(amount, 0);
+        this.setMoney(this.getMoney() + amountToDeposit);
+        return amountToDeposit;
+    }
+    withdraw(amount){
+        const currentMoney = this.getMoney();
+        const amountToWithdraw = Math.min(Math.max(amount, 0), currentMoney);
+        this.setMoney(this.getMoney() - amountToWithdraw);
+        return amountToWithdraw;
+    }
+}
