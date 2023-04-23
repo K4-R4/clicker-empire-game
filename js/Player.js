03e2fd07f09c5431b5fb566ecaada29cdfaef575
K4-R4 <mabdman512@gmail.com>
2023-04-23 20:37:20 +0900

    feat/player-class: added player class

---
+++ js/Player.js
@@ -0,0 +1,10 @@
+class Player{
+    constructor(name, age, wagePerClick, dailyWage){
+        this.name = name;
+        this.age = age;
+        this.wagePerClick = wagePerClick;
+        this.dailyWage = dailyWage;
+    }
+}
+
+export { Player };
